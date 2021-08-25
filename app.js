85a3728449b3f8dc154609ce96ab6ddd16b3031b
santiago <santigorbe@gmail.com>
2021-08-25 16:49:54 -0300

    v1

--- app.js
+++ app.js
@@ -3,10 +3,13 @@
 const express = require('express');
 const mysql = require('mysql');
 const cors = require('cors');
-
+// const jwt = require('jsonwebtoken');
+require('dotenv').config();
 let app = express();
 
+
 app.use(express.json());
+app.use(express.urlencoded({extended:false}));
 //Cors para poder pedir la informacion desde una pagina externa 
 app.use(cors());
 //Armamos la conexion
@@ -17,6 +20,7 @@ var conexion = mysql.createConnection({
     database:'articulosdb'
 });
 
+
 conexion.connect(function(err){
     if(err){
         throw err;
@@ -24,11 +28,63 @@ conexion.connect(function(err){
         console.log('Conexion con la bd exitosa')
     }
 });
+
 app.get('/', function(req,res){
-    res.send('Api de Santiago Gorbea');    
+    res.send('Api de Santiago Gorbea.');    
 });
+
+// app.get('/login', (req,res)=>{
+//     res.send(`
+//     <html>
+//     <head>
+//     <title>Login</title>
+//     </head>
+//     <body>
+//     <form method='POST' action='/auth'>
+//         Nombre de usuario:<input type='text' name='text'/> <br/>
+//         Contraseña:<input type='password' name='password'/> <br/>
+//         <input type='submit' value='Iniciar seción'/>
+//     </form>
+//     </body>
+//     </html>   
+//     `)
+// });
+// app.post('/auth', (req,res) =>{
+//     const {username,password} = req.body;
+//     //Consulatar a la base de datos
+    
+//     const user = {username:username};
+    
+//     const accessToken = generateAccessToken(user);
+    
+//     res.header('authorization', accessToken).json({
+//         message:'Usuario autenticado',
+//         token: accessToken
+//     });
+//     });
+    
+//     //Nos pide la info a encriptar y la clave para desencriptarla
+//     function generateAccessToken(user){
+//         return jwt.sign(user, process.env.token, {expiresIn: '5m'});
+//     }
+    
+//     function validateToken(req,res,next){
+//         const accessToken = req.headers['authorization'] || req.query.accessToken;
+//         if(!accessToken) res.send('Access denied');
+//         jwt.verify(accessToken, process.env.token, (err,user) =>{
+//             if(err){
+//                 res.send('Access denied, token expired or incorrect');
+//             }else{
+//             req.user = user;
+//             next();
+//             }
+//         })
+//     }
+
+
+
 app.listen('3000', function(){
-    console.log('Servidor iniciado ' );
+    console.log('Servidor iniciado en puerto 3000 ' );
 });
 
 //Operaciones de la api
@@ -68,7 +124,7 @@ app.post('/api/articulos', (req,res) =>{
 });
 
 //Editar articulos
-app.put('/api/articulos/:id',(req,res)=>{
+app.put('/api/articulos/:id' ,(req,res)=>{
     let id = req.params.id;
     let descripcion = req.body.descripcion;
     let precio = req.body.precio;
@@ -94,5 +150,3 @@ conexion.query('DELETE FROM articulos WHERE id = ?', [req.params.id], (err,filas
         }
     });
 });
-
-//Agregar el cors para que pueda usarlo con otras paginas 
